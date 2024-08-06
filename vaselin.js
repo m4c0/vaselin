b4d6ac7138f2af94e8f39c9c598ba25365bf76e9
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-06 11:21:36 +0100

    exposing ptr-to-str utility

--- vaselin.js
+++ vaselin.js
@@ -1,10 +1,13 @@
+function vaselin_tostr(ptr, size) {
+  return new TextDecoder().decode(new Uint8Array(leco_exports.memory.buffer, ptr, size));
+}
+
 (function() {
   function ifn(fn) { return leco_exports.__indirect_function_table.get(fn); }
-  function arr(ptr, size) { return new Uint8Array(leco_exports.memory.buffer, ptr, size); }
 
   leco_imports.vaselin = {
-    console_error : (ptr, size) => console.error(new TextDecoder().decode(arr(ptr, size))),
-    console_log : (ptr, size) => console.log(new TextDecoder().decode(arr(ptr, size))),
+    console_error : (ptr, size) => console.error(vaselin_tostr(ptr, size)),
+    console_log : (ptr, size) => console.log(vaselin_tostr(ptr, size)),
     date_now : () => BigInt(Date.now()),
     request_animation_frame : (fn) => window.requestAnimationFrame(ifn(fn)),
     set_timeout : (fn, timeout) => setTimeout(ifn(fn), timeout),
